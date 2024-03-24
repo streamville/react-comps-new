b0e932b5c60229f08ffa9e613fc421bfed56ffa1
Dennie Nguyen <50893065+streamville@users.noreply.github.com>
2024-03-24 17:46:25 -0400

    refactor using useReducer

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -1,28 +1,86 @@
 import Button from '../components/Button';
-import { useState } from 'react';
+import { useReducer } from 'react';
 import Panel from '../components/Panel';
 
+const INCREMENT_COUNT = 'increment';
+const SET_MAGIC_NUMBER = 'setMagicNumber';
+const DECREMENT_COUNT = 'decrement';
+const ADD_MAGIC_NUMBER = 'addMagicNumber';
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case INCREMENT_COUNT:
+      return {
+        ...state,
+        count: state.count + 1
+      };
+      case DECREMENT_COUNT:
+        return {
+          ...state,
+          count: state.count - 1
+        };
+    case SET_MAGIC_NUMBER:
+      return {
+        ...state,
+      magicNumber: action.payload,
+    };
+    case ADD_MAGIC_NUMBER:
+      return {
+        ...state,
+        count: state.count + state.magicNumber,
+        magicNumber: 0
+      };
+    default:
+      return state;
+  
+  }
+}
 
 function CounterPage({ initialCount }) {
-  const [ count, setCount ] = useState(initialCount);
-  const [ magicNumber, setMagicNumber ] = useState(0);
+  // const [ count, setCount ] = useState(initialCount);
+  // const [ magicNumber, setMagicNumber ] = useState(0);
+  const [state, dispatch] = useReducer(reducer, {
+    count: initialCount,
+    magicNumber: 0
+  });
+
 
   const increment = () => {
-    setCount(count + 1);
+    dispatch({
+      type: INCREMENT_COUNT
+    });
   }
 
   const decrement = () => {
-    setCount(count - 1);
+    dispatch({
+      type: DECREMENT_COUNT
+    });
   }
-  const addMagicNumber = (e) => {
+
+
+  const setMagicNumber = (e) => {
     const value = parseInt(e.target.value) || 0;
-    setMagicNumber(value);
+
+    dispatch({
+      type: SET_MAGIC_NUMBER,
+      payload: value
+    })
   }
+  const handleChange = (e) => {
+    const value = parseInt(e.target.value) || 0;
+
+    dispatch({
+      type: SET_MAGIC_NUMBER,
+      payload: value
+    });
+  }
+  
   const handleSubmit = (e) => {
     e.preventDefault();
-    setCount(count + magicNumber);
-    setMagicNumber(0);
+
+    dispatch({
+      type: ADD_MAGIC_NUMBER
+    })
   }
 
 
@@ -32,15 +90,15 @@ function CounterPage({ initialCount }) {
       <h1 className="text-4xl">Counter</h1>
       <div className="flex space-x-4">
         <Button onClick={decrement}>-</Button>
-        <span className="text-2xl">{count}</span>
+        <span className="text-2xl">{state.count}</span>
         <Button onClick={increment}>+</Button>
       </div>
 
       <form onSubmit={handleSubmit} className='flex justify-center items-center'>
         <label className='text-xl'>Add magic numbers: </label>
         <input 
-          onChange={addMagicNumber}
-          value={magicNumber || ''}
+          onChange={handleChange}
+          value={state.magicNumber || ''}
           type="number" 
           className='p-1 m-3 bg-gray-100 rounded'
         /> 
